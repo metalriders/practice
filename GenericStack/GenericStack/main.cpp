c5880d701f0cc52ca88b36aaf3e981bff04cb327
metalriders <daniboy307@outlook.com>
2017-02-01 13:16:54 -0600

    Use of copy constructor

--- GenericStack/GenericStack/main.cpp
+++ GenericStack/GenericStack/main.cpp
@@ -4,8 +4,9 @@
 int main(int argv[], char** argc)
 {
 	g_stack<char> c_stack = g_stack<char>();
-	g_stack<char> c_stack2 = g_stack<char>();
+	g_stack<char> c_stack2(c_stack);
 	g_stack<int> i_stack = g_stack<int>();
+
 	std::cout << c_stack.num_instances() << "\n";
 	std::cout << i_stack.num_instances() << "\n";
 	return 0;
