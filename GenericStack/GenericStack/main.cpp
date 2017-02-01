a44825f6e91566aa9a68422c4f5cf86a73129f8e
metalriders <daniboy307@outlook.com>
2017-02-01 16:00:28 -0600

    updated main with GetInstancesQty refactor

--- GenericStack/GenericStack/main.cpp
+++ GenericStack/GenericStack/main.cpp
@@ -3,13 +3,11 @@
 
 int main(int argv[], char** argc)
 {
-	g_stack<char>::SetInstances(12);
-
 	g_stack<char> c_stack = g_stack<char>();
 	g_stack<char> c_stack2(c_stack);
 	g_stack<int> i_stack = g_stack<int>();
 
-	std::cout << c_stack.num_instances() << "\n";
-	std::cout << i_stack.num_instances() << "\n";
+	std::cout << c_stack.GetInstancesQty() << "\n";
+	std::cout << i_stack.GetInstancesQty() << "\n";
 	return 0;
 }
