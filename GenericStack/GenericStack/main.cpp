b66ff2da8b32435db612daba4bf01e790c6e2134
metalriders <daniboy307@outlook.com>
2017-02-01 12:45:09 -0600

    Diff generic class counter

--- GenericStack/GenericStack/main.cpp
+++ GenericStack/GenericStack/main.cpp
@@ -3,7 +3,10 @@
 
 int main(int argv[], char** argc)
 {
-	g_stack<char> stack = g_stack<char>();
-	std::cout << stack.num_instances();
+	g_stack<char> c_stack = g_stack<char>();
+	g_stack<char> c_stack2 = g_stack<char>();
+	g_stack<int> i_stack = g_stack<int>();
+	std::cout << c_stack.num_instances() << "\n";
+	std::cout << i_stack.num_instances() << "\n";
 	return 0;
 }
