2f8bcd52255b2bc936ef9dc4bbe5a6ce6cc1c76d
metalriders <daniboy307@outlook.com>
2017-02-02 23:07:04 -0600

    Added TypeId project

---
+++ Learning/TypeId usage/Person.h
@@ -0,0 +1,16 @@
+#ifndef _PERSON_H_
+#define	_PERSON_H_
+
+#include <string>
+#include <iostream>
+
+class Person
+{
+	std::string name;
+public:
+	Person();
+	~Person();
+
+};
+
+#endif // !_PERSON_H_
