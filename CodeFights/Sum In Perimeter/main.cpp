ff4e34050e29efe6077ba4000d6f015b3af976fb
metalriders <daniboy307@outlook.com>
2017-02-14 13:35:22 -0600

    Idea for a beam search

--- CodeFights/Sum In Perimeter/main.cpp
+++ CodeFights/Sum In Perimeter/main.cpp
@@ -7,9 +7,9 @@ struct Cell
 {
 	int direction;
 	int* data_ptr;
+	std::pair<int,int> coords;
 	bool visited[4];
 	bool operator==(Cell &rhs) { return this->data_ptr == rhs.data_ptr; }
-
 };
 
 enum DirectionType
@@ -34,19 +34,19 @@ vector_cell getNeigbors(grid_t &grid, pair_int_int bounds, pair_int_int coords)
 	//	search biggest 2 neighbors and add them to the stack (that are not already in our visited nodes)
 	if (up >= 0)
 	{
-		neigbors.push_back(Cell{ DirectionType::up, &grid[up][coords.second] });
+		neigbors.push_back(Cell{ DirectionType::up, &grid[up][coords.second], pair_int_int(up, coords.second) });
 	}
 	if (down <= bounds.first - 1)
 	{
-		neigbors.push_back(Cell{ DirectionType::down, &grid[down][coords.second] });
+		neigbors.push_back(Cell{ DirectionType::down, &grid[down][coords.second], pair_int_int(down, coords.second) });
 	}
 	if (left >= 0)
 	{
-		neigbors.push_back(Cell{ DirectionType::left, &grid[coords.first][left] });
+		neigbors.push_back(Cell{ DirectionType::left, &grid[coords.first][left], pair_int_int(coords.first, left) });
 	}
 	if (right <= bounds.second - 1)
 	{
-		neigbors.push_back(Cell{ DirectionType::right, &grid[coords.first][right] });
+		neigbors.push_back(Cell{ DirectionType::right, &grid[coords.first][right], pair_int_int(coords.first, right) });
 	}
 
 	std::sort(neigbors.begin(), neigbors.end(), [](Cell a, Cell b) { return *a.data_ptr > *b.data_ptr; });
@@ -126,15 +126,16 @@ int sumInPerimeter(grid_t grid, int p) {
 	pair_int_int coords;
 	pair_int_int bounds(0, 0);
 	int max = INT_MIN;
-	std::vector<int*> grid_line;
-
-	// get biggest number coordinates of it
+	vector_cell grid_line;
+	
+	// reduce number of start points by picking values >= 0
 	for (auto row = grid.begin(); row != grid.end(); row++, bounds.first++)
 	{
 		bounds.second = 0;
 		for (auto col = (*row).begin(); col != (*row).end(); col++, bounds.second++)
 		{
-			grid_line.push_back(&grid[bounds.first][bounds.second]);
+			if(grid[bounds.first][bounds.second] >= 0)
+				grid_line.push_back(Cell{ DirectionType::undef, &grid[bounds.first][bounds.second], bounds });
 			if (*col > max)
 			{
 				max = *col;
@@ -150,26 +151,27 @@ int sumInPerimeter(grid_t grid, int p) {
 
 	printf("Matrix of %d x %d\n", bounds.first, bounds.second);
 	printf("Index of max value > %d, %d\n", coords.first, coords.second);
-
-
-
-	// while something
+	
+	// Search Paths 
 	for (auto item : grid_line)
-	{	
-		vector_cell to_process;
+	{
 		vector_cell curr_neigbors;
+		vector_cell to_process;
 		vector_cell path;
+		coords = item.coords;
 
-		path.push_back(Cell{ DirectionType::undef, item });
+		path.push_back(Cell{ DirectionType::undef, item.data_ptr });
 		sum = *path[0].data_ptr;
 		perimeter = 4;
 
+		// Beam search of 3 nodes maybe?
 		while (true)
 		{
+			if (path.empty()) break;
 			to_process.clear();
 			if (perimeter > p || no_moves)
 			{
-				// pop current node
+				// pop current node if not empty
 				Cell to_pop = *path.rbegin();
 				path.pop_back();
 
@@ -180,7 +182,7 @@ int sumInPerimeter(grid_t grid, int p) {
 
 				continue;
 			}
-			else if (perimeter < p && path.size() < p / 2)
+			else if (perimeter < p || path.size() < (p / 2)-1)
 			{
 				curr_neigbors = getNeigbors(grid, bounds, coords);
 
@@ -202,9 +204,12 @@ int sumInPerimeter(grid_t grid, int p) {
 					current_cell->visited[cell.direction] = true;
 					cell.visited[reverseCoord(cell.direction)] = true;
 					path.push_back(cell);
+					
 					updateCoords(cell.direction, coords);
-					perimeter += updatePerimeter(grid, path, bounds, coords);
-					sum += *cell.data_ptr;
+					if (cell.data_ptr != (*path.begin()).data_ptr) {
+						perimeter += updatePerimeter(grid, path, bounds, coords);
+						sum += *cell.data_ptr;
+					}
 
 					no_moves = false;
 					break;
@@ -226,15 +231,23 @@ int main(int argc[], char** arcgv)
 	p = 6;
 	std::cout << "Max sum is: " << sumInPerimeter(grid, p) << "\n\n";
 
+	// Test case #5
+	grid = { { 1,1,1,1,1,1 },{ 1,5,2,1,1,1 },{ 0,3,2,1,1,1 },{ 1,2,4,1,1,1 } };		// needs to be 12 :(
+	p = 8;
+	std::cout << "Max sum is: " << sumInPerimeter(grid, p) << "\n\n";
+
 	// Test case #11
 	grid = { { -1,-1,0,1 }, { 0,5,2,-1 }, { -1,0,1,1 } };		// passed
 	p = 10;
 	std::cout << "Max sum is: " << sumInPerimeter(grid, p) << "\n\n";
 
 	// Test case #15
-	grid = { { 1, -1, 1, -1 }, { 1,0,5,10 }, { 1,0,0,4 } };		// needs to be 20 :(
+	grid = { { 1, -1, 1, -1 }, { 1,0,5,10 }, { 1,0,0,4 } };		// passed
 	p = 10;
 	std::cout << "Max sum is: " << sumInPerimeter(grid, p) << "\n\n";
 
+	
+
+	
 	return 0;
 }
